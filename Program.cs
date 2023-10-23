169c4b3b440b02eaf0f94905240715a6638181f5
Jupiteries <128291249+Jupiteries@users.noreply.github.com>
2023-10-23 14:47:06 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace ShoppingCentre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+            using (FileStream overallBlackList = new FileStream("overallblacklist.json", FileMode.OpenOrCreate));
+            Client client = new Client();
+
+            Shopping.Welcome(client);
+        }
+    }
+}
